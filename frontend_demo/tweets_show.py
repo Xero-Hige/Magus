b46d790f29c35626051a3fe5056e62a0382c3989
Xero-Hige <gaston.martinez.90@gmail.com>
2017-01-09 00:18:29 -0300

    Fixed bug showing tweets

--- frontend_demo/tweets_show.py
+++ frontend_demo/tweets_show.py
@@ -1,4 +1,5 @@
 import os
+import re
 from math import ceil
 
 from flask import Flask, render_template, request
@@ -8,6 +9,18 @@ PINED_ALERTS_PER_PAGE = 18
 app = Flask(__name__)
 import json
 
+REMOJIS = re.compile(u"("
+                     u"(\ud83d[\ude00-\ude4f])|"  # emoticons
+                     u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
+                     u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
+                     u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
+                     u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
+                     u"+"
+                     u")", flags=re.UNICODE)
+
+EMOJIS = re.compile(u"\\ud83d", re.UNICODE)
+
+print (EMOJIS)
 
 @app.route('/', methods=["GET", "POST"])
 def root():
@@ -15,10 +28,10 @@ def root():
     tweets.sort()
     page = int(request.form.get("PAGE", "0"))
 
+    pages = ceil(len(tweets) / PINED_ALERTS_PER_PAGE)
+
     tweets = [load_tweet(tweets[i]) for i in range(PINED_ALERTS_PER_PAGE * page,
                                                    min(PINED_ALERTS_PER_PAGE * (page + 1), len(tweets)))]
-
-    pages = ceil(len(tweets) / PINED_ALERTS_PER_PAGE)
 
     return render_template("index.html", pagename="Twitter", tweets=tweets, page=page, pages=pages)
 
@@ -35,10 +48,15 @@ def load_tweet(tweet_file_name):
     tweet = {}
     with open("../tweets/" + tweet_file_name) as tweet_file:
         tweet_dump = tweet_file.read()
+
     loaded_tweet = json.loads(tweet_dump, encoding="utf-8")
 
     tweet["tweet_id"] = tweet_file_name.replace(".json", "")
     tweet["tweet_text"] = loaded_tweet.get("text", "")
+
+    print (re.findall(EMOJIS, tweet["tweet_text"]))
+    tweet["tweet_text"] = re.sub(EMOJIS, r'<span class="emoji" data-emoji="\1"></span>', tweet["tweet_text"])
+
     tweet["tweet_lang"] = loaded_tweet.get("lang", "")
     if (tweet.get("place")):
         tweet["tweet_place"] = loaded_tweet.get("place", {}).get("name", "")
