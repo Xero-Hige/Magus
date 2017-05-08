cc1c78ad7afe5481d369dcd2ef1fb2408dda84b6
Xero-Hige <gaston.martinez.90@gmail.com>
2017-05-08 19:58:59 -0300

    Added missing word preprocessors

--- src/new_tweeter_process.py
+++ src/new_tweeter_process.py
@@ -1,14 +1,16 @@
 import os
 
-
 def get_preprocessors(excluded=[]):
-    lst = os.listdir("tokenizers")
+    lst = os.listdir("word_preprocessor")
 
     modules = []
 
     for x in lst:
         x = x.replace(".py", "")
-        x = "preprocess." + x
+        x = "word_preprocessor." + x
         modules.append(__import__(x, fromlist=['']))
 
     return [module for module in modules if "__" not in module.__name__ and module.__name__ not in excluded]
+
+
+print (get_preprocessors())
--- src/word_preprocessor/preprocess_user.py
+++ src/word_preprocessor/preprocess_user.py
@@ -1,12 +1,8 @@
-import re
-
-URL_REGEXP = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-
 def preprocess(word):
     """ """
     updated = False
-    if re.match(URL_REGEXP,word):
-        word = " URL "
+    if word[0] == "@":
+        word = " @USER "
         updated = True
 
     return word, updated
