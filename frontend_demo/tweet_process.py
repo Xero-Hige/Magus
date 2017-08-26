6e80aa6e8e8c92100c9414b64876d37b523f8fc6
Xero-Hige <gaston.martinez.90@gmail.com>
2017-08-26 03:03:22 -0300

    Changed how links and user are shown in the tagger

--- frontend_demo/tweet_process.py
+++ frontend_demo/tweet_process.py
@@ -3,12 +3,12 @@ import re
 
 def censor_urls(text):
     for url in re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text):
-        text = text.replace(url, " URL ")
+        text = text.replace(url, "<a href='#'>http://URL</a>")
     return text
 
 
 def anonymize_usernames(text):
     for word in text.split():
         if len(word) > 1 and word[0] == '@' and word[1].isalpha():
-            text = text.replace(word, " USER ")
+            text = text.replace(word, "<b>@USER</b>")
     return text
