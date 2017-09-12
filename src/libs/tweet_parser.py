6f10052a511d66aae13743b535257492f1121159
Hige <Gaston.martinez.90@gmail.com>
2017-09-12 01:12:27 -0300

    Fixed parser error (still not full working)

--- src/libs/tweet_parser.py
+++ src/libs/tweet_parser.py
@@ -1,28 +1,30 @@
 import json as Serializer
 
-
 class TweetParser:
 
 	@staticmethod
 	def parse_from_json_file(filename):
-
 		with open(filename,'r') as _file:
-			return self.parse_from_json_string(_file.read())
+			return TweetParser.parse_from_json_string(_file.read())
 
+	@staticmethod
 	def parse_from_json_string(json_string):
 		tweet = Serializer.loads(json_string)
 
 		if not "user" in tweet:
-            return None
+			return None
 
 		tweet_dict = {}
 
-		tweet_dict["at_user"] = tweet_dict["user"]["screen_name"]
-        tweet_dict["display_name"] = tweet_dict["user"]["name"].title()
-        tweet_dict["user_location"] = tweet_dict["user"]["location"]
-        tweet_dict["user_image"] = tweet_dict["user"]["profile_image_url"].replace("_normal.jpg", ".jpg")
-        tweet_dict["user_back"] = tweet_dict["user"].get("profile_banner_url", " ")
+		tweet_dict["at_user"] = tweet["user"]["screen_name"]
+		tweet_dict["display_name"] = tweet["user"]["name"].title()
+		tweet_dict["user_location"] = tweet["user"]["location"]
+		tweet_dict["user_image"] = tweet["user"]["profile_image_url"].replace("_normal.jpg", ".jpg")
+		tweet_dict["user_back"] = tweet["user"].get("profile_banner_url", " ")
 
-		tweet_dict["text"] = tweet_dict["text"].encode("utf-8", 'replace').decode("utf-8")
+		if "truncated" in tweet and "extended_tweet" in tweet:
+			tweet_dict["text"] = tweet["extended_tweet"]["full_text"].encode("utf-8", 'replace').decode("utf-8")
+		else:
+			tweet_dict["text"] = tweet["text"].encode("utf-8", 'replace').decode("utf-8")
 
-		return tweet_dict 
+		return tweet_dict
