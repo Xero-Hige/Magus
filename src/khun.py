6590d74dd8b14def7841719a7d0f8c919ea4fe68
Xero-Hige <gaston.martinez.90@gmail.com>
2017-09-22 17:37:07 -0300

    Added core launching object

--- src/khun.py
+++ src/khun.py
@@ -1,4 +1,5 @@
 import os
+import signal
 
 from m_cores import fetcher_core
 
@@ -7,13 +8,40 @@ CORES = {
 }
 
 
-def main():
-    pid = os.fork()
-    if pid == 0:
-        result = CORES["fectcher"]()
-        exit(result)
+class DetachedCore():
+    def __init__(self, main_function):
+        pid = os.fork()
+        if pid == 0:
+            result = main_function()
+            exit(result)
+        else:
+            self.pid = pid
 
-    else:
+    def stop(self):
+        if self.pid != 0:
+            os.kill(self.pid, signal.SIGTERM)
+
+    def dispatch_signal(self, signal):
+        if self.pid != 0:
+            os.kill(self.pid, signal.SIGTERM)
+
+
+def main():
+    cores = []
+
+    for _ in range(1):
+        core = DetachedCore(CORES["fectcher"])
+        cores.append(core)
+
+    def handler(signum, frame):
+        print ("handling: ", signum)
+        for _core in cores:
+            _core.stop()
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+
+    for _ in range(len(cores)):
         os.wait()
 
     print ("Ended")
