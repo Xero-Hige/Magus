888cc59855c9de664333ed1f427008959a1bb9a5
Xero-Hige <gaston.martinez.90@gmail.com>
2017-09-25 03:53:47 -0300

    Added cleaner core

---
+++ src/m_cores/cleaner_core.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import pickle as Serializer
+
+from core_utils.debugger import debug_core_print_d
+from to_check.RabbitHandler import *
+
+
+def main(tag, worker_number, input_queue, output_queue):
+    reader = RabbitHandler(input_queue)
+    writer = RabbitHandler(output_queue)
+
+    def callback(tweet_string):
+        if not tweet_string:
+            return
+
+        tweet = Serializer.loads(tweet_string)
+
+        if not tweet:
+            return
+
+        debug_core_print_d(tag, worker_number, "Cleaning tweet")
+
+        writer.send_message(Serializer.dumps(tweet))
+
+    reader.receive_messages(callback)
+
+
+if __name__ == '__main__':
+    main()
