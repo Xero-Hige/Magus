f273e13d32978c54832023e3d74d063fdf428643
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-01 19:49:48 -0300

    Reformat file

--- src/libs/neural_classifier.py
+++ src/libs/neural_classifier.py
@@ -1,11 +1,14 @@
+import random
+
 INPUT_SIZE = 3
 INPUT_LEN = 3
 OUTPUT_LEN = 9
 
-class NeuralClassifier():
 
+class NeuralClassifier():
     def __init__(self):
         pass
 
-    def classify(self,input_matrix):
-        return [ random.random() for _ in range(OUTPUT_LEN) ]
+    def classify(self, input_matrix):
+        # Baseline
+        return [random.random() for _ in range(OUTPUT_LEN)]
