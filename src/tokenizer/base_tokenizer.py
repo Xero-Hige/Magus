a1af79edd3decb86db5ed753e9dd4e6e0b84827d
Hige <Gaston.martinez.90@gmail.com>
2017-10-04 17:20:35 -0300

    Added base tokenizer

---
+++ src/tokenizer/base_tokenizer.py
@@ -0,0 +1,11 @@
+
+class Tokenizer():
+    """Defines a tokenizer class to parse tweets"""
+
+    @static_method
+    def tokenize(tweet):
+        """Returns a list of strings, being each string a token of the given
+        parsed tweet. Each token appears in the relative order inside the
+        tweet given by the tokenize method"""
+        
+        raise NotImplementedError
