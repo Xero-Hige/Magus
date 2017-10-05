08ed9f064ae11a8d99ac67b8631553ff81202731
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-05 00:59:18 -0300

    Fixed code errors at sentence yield (TODO: Give a better name)

--- src/libs/sentence_yield.py
+++ src/libs/sentence_yield.py
@@ -1,11 +1,16 @@
-class TrainSentenceGenerator():
+import os
 
-    def __init__(self,dirs,tokenizer):
+from libs.tweet_parser import TweetParser
+
+
+class TrainSentenceGenerator():
+    def __init__(self, dirs, tokenizer):
         self.dirs = dirs
         self.tokenizer = tokenizer
 
     def __iter__(self):
         for directory in self.dirs:
-            for file in os.walk(directory):
-                path = os.path.join(directory,file)
-                #load
+            for tweet_file in os.walk(directory):
+                path = os.path.join(directory, tweet_file)
+                tweet = TweetParser.parse_from_json_file(path)
+                yield self.tokenizer.tokenize(tweet)
