526c59b4a3dcf4c47f77bbfd8952bf01b874d9fc
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-08 02:54:58 -0300

    Added base w2v generator

--- src/embeddings_generator.py
+++ src/embeddings_generator.py
@@ -1,1 +1,15 @@
+import gensim
+
+from libs.sentence_yield import TrainSentenceGenerator
+from tokenizer.word_tokenizer import WordTokenizer
+
+
 def generate_embeddings():
+    sentences = TrainSentenceGenerator(["../tweets", "../bulk"], WordTokenizer)
+    model = gensim.models.Word2Vec(sentences, min_count=1)
+
+    a = model.most_similar(positive=['macri'], topn=2)
+    print (a)
+
+
+generate_embeddings()
