7a427b748cc616f7c5756db025a1d312f21c3512
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-08 03:22:23 -0300

    Fixed yield error at os.walk

--- src/libs/sentence_yield.py
+++ src/libs/sentence_yield.py
@@ -10,7 +10,9 @@ class TrainSentenceGenerator():
 
     def __iter__(self):
         for directory in self.dirs:
-            for tweet_file in os.walk(directory):
-                path = os.path.join(directory, tweet_file)
-                tweet = TweetParser.parse_from_json_file(path)
-                yield self.tokenizer.tokenize(tweet)
+            for _, _, tweet_files in os.walk(directory):
+                for tweet_file in tweet_files:
+                    path = os.path.join(directory, tweet_file)
+                    tweet = TweetParser.parse_from_json_file(path)
+                    tweet["tweet_text"] = tweet["tweet_text"].lower()
+                    yield self.tokenizer.tokenize(tweet)
