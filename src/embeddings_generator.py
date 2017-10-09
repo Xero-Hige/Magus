bcb364c52d4b2e37f57055cd1be4e3043566d79f
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-09 01:19:11 -0300

    Replaced model with word vector

--- src/embeddings_generator.py
+++ src/embeddings_generator.py
@@ -7,9 +7,9 @@ from tokenizer.word_tokenizer import WordTokenizer
 def generate_embeddings():
     sentences = TrainSentenceGenerator(["../tweets", "../bulk"], WordTokenizer)
     model = gensim.models.Word2Vec(sentences, min_count=1, size=300)
-
-    a = model.most_similar(positive=['fox'], topn=5)
+    word_vector = model.wv
+    a = word_vector.most_similar(positive=['fox'], topn=5)
     print (a)
-
+    word_vector.save('./mymodel.mdl')
 
 generate_embeddings()
