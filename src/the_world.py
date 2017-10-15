7e3965985bb022cb7c61e23347ddc2ef170d9eef
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-15 18:21:59 -0300

    Added base example of map rendering

--- src/the_world.py
+++ src/the_world.py
@@ -1,9 +1,16 @@
+import random
 import re
 
 from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 
+from gmaps_rectangle import GmapsRectangle
+from grid_region import GridRegion
 from libs.locations import get_coordinates_from_code
+
+COLUMNS = 100
+ROWS = 200
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -20,4 +27,29 @@ GRIDS = {}
 def map_show():
     # GRIDS[0] = MapGrid()
     W, S, E, N = get_coordinates_from_code("AR")
-    return render_template("map.html", lat=(S + N) / 2, long=(W + E) / 2)
+
+    rectangles = []
+    row_size = (S - N) / ROWS
+    col_size = (W - E) / COLUMNS
+
+    for row in range(ROWS):
+        for column in range(COLUMNS):
+            north = N + row_size * row
+            south = N + row_size * (row + 1)
+            east = E + col_size * column
+            west = E + col_size * (column + 1)
+            rect = GmapsRectangle(north=north, south=south, west=west, east=east)
+            rect.set_colors(random.choice([GridRegion.SAD, GridRegion.HAPPY, GridRegion.INDIFERENT, GridRegion.ANGRY]),
+                            random.choice([0.3, 0.5, 0.8, 1]))
+            rectangles.append(rect.get_renderable_info())
+
+    map = Map(
+            identifier="rectmap",
+            varname="rectmap",
+            lat=(S + N) / 2,
+            lng=(W + E) / 2,
+            rectangles=rectangles,
+            style="height:600px;width:800px;margin:0;"
+    )
+
+    return render_template("map.html", map=map)
