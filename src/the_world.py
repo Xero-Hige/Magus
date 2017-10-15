e15df761abe804e43633af360e413c1f863e6324
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-15 20:41:27 -0300

    Added grid singleton

--- src/the_world.py
+++ src/the_world.py
@@ -1,13 +1,11 @@
-import random
 import re
 
 from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 
-from gmaps_rectangle import GmapsRectangle
-from grid_region import GridRegion
 from libs.locations import get_coordinates_from_code
+from map_grid import MapGrid
 
 COLUMNS = 100
 ROWS = 200
@@ -20,36 +18,21 @@ GoogleMaps(app)
 
 EMOJIS = re.compile(u"\\ud83d", flags=re.UNICODE)
 
-GRIDS = {}
+W, S, E, N = get_coordinates_from_code("AR")
+GRID = MapGrid(north=N, south=S, east=E, west=W, grid_rows=200, grid_columns=100)
 
 
 @app.route('/', methods=["GET"])
 def map_show():
-    # GRIDS[0] = MapGrid()
-    W, S, E, N = get_coordinates_from_code("AR")
-
-    rectangles = []
-    row_size = (S - N) / ROWS
-    col_size = (W - E) / COLUMNS
-
-    for row in range(ROWS):
-        for column in range(COLUMNS):
-            north = N + row_size * row
-            south = N + row_size * (row + 1)
-            east = E + col_size * column
-            west = E + col_size * (column + 1)
-            rect = GmapsRectangle(north=north, south=south, west=west, east=east)
-            rect.set_colors(random.choice([GridRegion.SAD, GridRegion.HAPPY, GridRegion.INDIFERENT, GridRegion.ANGRY]),
-                            random.choice([0.3, 0.5, 0.8, 1]))
-            rectangles.append(rect.get_renderable_info())
-
+    rectangles = GRID.get_status()
     map = Map(
             identifier="rectmap",
             varname="rectmap",
             lat=(S + N) / 2,
             lng=(W + E) / 2,
             rectangles=rectangles,
-            style="height:600px;width:800px;margin:0;"
+            style="height:600px;width:800px;margin:0;",
+            zoom=7
     )
 
     return render_template("map.html", map=map)
