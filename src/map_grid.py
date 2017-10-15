86d0a8b05f273c0bf513a5a4c57a5a53103ab6b0
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-15 17:01:24 -0300

    Changed grid to handle rows and columns

--- src/map_grid.py
+++ src/map_grid.py
@@ -12,20 +12,21 @@ class MapGrid():
                              "ANGRY": GridRegion.ANGRY,
                              "INDIFFERENT": GridRegion.INDIFERENT}
 
-    def __init__(self, coordinates_tl, coordinates_dr, grid_size=10):
-        self.grid_size = grid_size
-        self.grid = [[GridRegion() for _ in range(grid_size)] for _ in range(grid_size)]
+    def __init__(self, coordinates_tl, coordinates_dr, grid_rows=10, grid_columns=10):
+        self.grid_rows = grid_rows
+        self.grid_columns = grid_columns
+        self.grid = [[GridRegion() for _ in range(grid_columns)] for _ in range(grid_rows)]
 
         reader = RabbitHandler("output")
 
         reader.receive_messages(self.__update)
 
         latitude_distance = coordinates_dr[0] - coordinates_tl[0]
-        self.latitude_pass = latitude_distance / grid_size
+        self.latitude_pass = latitude_distance / grid_rows
         self.latitude_init = coordinates_tl[0]
 
         longitude_distance = coordinates_dr[1] - coordinates_tl[1]
-        self.longitude_pass = longitude_distance / grid_size
+        self.longitude_pass = longitude_distance / grid_columns
         self.longitude_init = coordinates_tl[1]
 
     def get_status(self):
