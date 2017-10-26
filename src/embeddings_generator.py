73219136ba5c24d66510dd46c652360655a4370c
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-26 00:12:56 -0300

    Removed debug code

--- src/embeddings_generator.py
+++ src/embeddings_generator.py
@@ -6,10 +6,8 @@ from tokenizer.word_tokenizer import WordTokenizer
 
 def generate_embeddings():
     sentences = TrainSentenceGenerator(["../tweets", "../bulk"], WordTokenizer)
-    model = gensim.models.Word2Vec(sentences, min_count=3, size=300)
+    model = gensim.models.Word2Vec(sentences, min_count=2, size=300)
     word_vector = model.wv
-    a = word_vector.most_similar(positive=['macri'], topn=10)
-    print(a)
     word_vector.save('./mymodel.mdl')
 
 
