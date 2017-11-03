1abb5dda0d60c0debdb9fc45f62dd1d808655b38
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-03 03:48:28 -0300

    Added validator

--- src/new_interface.py
+++ src/new_interface.py
@@ -4,6 +4,7 @@ import random
 from flask import Blueprint, redirect, render_template, request
 
 from libs.db_tweet import DB_Handler
+from libs.lexicon import Lexicon
 from libs.tweet_anonymize import full_anonymize_tweet
 from libs.tweet_parser import TweetParser
 
@@ -11,6 +12,8 @@ APP_ROUTE = '/en'
 
 new_interface = Blueprint('new_interface', __name__,
                           template_folder='templates')
+
+LEXICONS = [Lexicon("./lexicons/en_lexicon.lxc", lang="en")]
 
 
 @new_interface.route(APP_ROUTE + '/classify', methods=["GET"])
@@ -21,6 +24,43 @@ def classify_get():
     tweet = load_tweet(random.choice(tweets))
 
     return render_template("catalog_alternative.html", tweet=tweet, max=max, app_route=APP_ROUTE)
+
+
+@new_interface.route(APP_ROUTE + '/validate', methods=["GET"])
+def validate_tag_get():
+    tweets = ["../tweets/{}".format(x) for x in os.listdir("../tweets")] \
+             + ["../bulk/{}".format(x) for x in os.listdir("../bulk")]
+
+    tweet = load_tweet(random.choice(tweets))
+
+    if tweet["totals"] > 1:
+        classification = tweet["max_emotion"]
+        auto = "BD"
+    else:
+        classification = LEXICONS[0].auto_tag_sentence(tweet["tweet_text"]) if LEXICONS[0].get_associated_lang() in \
+                                                                               tweet[
+                                                                                   "tweet_user_lang"].lower() else "neutral"
+        auto = "LEXICON " + LEXICONS[0].get_associated_lang()
+
+    return render_template("tag_validator.html", tweet=tweet, max=max, app_route=APP_ROUTE,
+                           classification=classification, auto=auto)
+
+
+@new_interface.route(APP_ROUTE + '/validate', methods=["POST"])
+def validate_tag_get():
+    validation = request.form["validation"]
+
+    if validation == "skip":
+        return redirect(APP_ROUTE + "/validate")
+
+    modify_value = 2 if validation == "yes" else -3
+
+    emotion = request.form["classification"]
+    tweet_id = request.form["tweet_id"]
+
+    classify_tweet_db(str(tweet_id), emotion, modify_value)
+
+    return redirect(APP_ROUTE + "/validate")
 
 
 @new_interface.route(APP_ROUTE + '/add_classification/<int:tweet_id>', methods=["GET"])
@@ -67,12 +107,22 @@ def load_tweet(tweet_file_name):
         _tweet = handler.get_tagged(tweet["tweet_id"])
 
         emotions = _tweet.get_emotions_list()
+        totals = _tweet.totals
 
     tweet_dict_add_emotions(tweet, emotions)
+    tweet["totals"] = totals
 
     return tweet
 
 
 def tweet_dict_add_emotions(tweet, emotions):
+    max_value = -1
+    max_emotion = None
     for emotion in emotions:
         tweet[emotion[1]] = emotion[0]
+
+        if max_value < emotion[0]:
+            max_value = emotion[0]
+            max_emotion = emotion[1]
+
+    tweet["max_emotion"] = max_emotion
