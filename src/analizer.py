e2890ebcb527361806e5dfa7e1a50137775cf9a4
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-15 15:04:41 -0300

    Updated analizer

--- src/analizer.py
+++ src/analizer.py
@@ -2,10 +2,11 @@ import os
 import random
 
 from libs.lexicon import Lexicon
+from libs.tweet_parser import TweetParser
 
-lexicon = Lexicon("lexicons/es_lexicon.lx","es")
+lexicon = Lexicon("lexicons/es_lexicon.lx", "es")
 
-for folder,_,files in os.walk("."):
+for folder, _, files in os.walk("./tweets_bashfull_old"):
     random.shuffle(files)
 
     emotion = folder.split("/")[-1]
@@ -13,30 +14,31 @@ for folder,_,files in os.walk("."):
 
     batch = 1
 
-    for _file in files[:2]:
-            tweet_id = _file.split(".")[0]
-            output_dir = os.path.join(".","results","batch{}".format(batch))
+    for _file in files[:10]:
+        tweet_id = _file.split(".")[0]
+        output_dir = os.path.join(".", "results", "batch{}".format(batch))
 
-            key_pressed = "y"
+        tweet = TweetParser.parse_from_json_file(os.path.join(folder, _file))
 
-            if key_pressed == "y":
-                output_dir = os.path.join(output_dir,"good")
-                try:
-                    os.makedirs(output_dir)
-                except:
-                    pass
+        key_pressed = input("{}: {}  Y/N".format(tweet_id, tweet[TweetParser.TWEET_TEXT]))
 
-                print(output_dir+"{}.json".format(tweet_id))
+        if key_pressed == "y":
+            output_dir = os.path.join(output_dir, "good")
+            try:
+                os.makedirs(output_dir)
+            except:
+                pass
+            print(output_dir + "{}.json".format(tweet_id))
 
-            elif key_pressed == "n":
-                output_dir = os.path.join(output_dir,"bad")
-                try:
-                    os.makedirs(output_dir)
-                except:
-                    pass
-                print(output_dir+"{}.json".format(tweet_id))
+        elif key_pressed == "n":
+            output_dir = os.path.join(output_dir, "bad")
+            try:
+                os.makedirs(output_dir)
+            except:
+                pass
+            print(output_dir + "{}.json".format(tweet_id))
 
-            else:
-                print("BOBO")
+        else:
+            print("BOBO")
 
-            print("<<{}>> {}".format(emotion,tweet_id))
+        print("<<{}>> {}".format(emotion, tweet_id))
