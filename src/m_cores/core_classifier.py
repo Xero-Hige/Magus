8bede5be8b82fa5304027b27fed4d39b9ca36ac1
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-19 16:23:26 -0300

    Reformat base core classifier

--- src/m_cores/core_classifier.py
+++ src/m_cores/core_classifier.py
@@ -12,16 +12,22 @@ from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 
 from libs.sentiments_handling import ANGER, ANTICIPATION, DISGUST, FEAR, JOY, NEUTRAL, SADNESS, SURPRISE, TRUST
-from libs.tweet_fetcher import TweetsFetcher
 from m_cores.magus_core import MagusCore
 
 EMOTION_LOOKUP = [JOY, TRUST, FEAR, SURPRISE, SADNESS, DISGUST, ANGER, ANTICIPATION, NEUTRAL]
+
+tf.app.flags.DEFINE_string('server', 'localhost:9000',
+                           'PredictionService host:port')
+FLAGS = tf.app.flags.FLAGS
 
 
 class ClassifierCore(MagusCore):
     def __init__(self, input_queue, output_queue, tag="Classifier", worker_number=0):
         MagusCore.__init__(self, tag, worker_number, input_queue, output_queue)
-        self.tweets_stream = TweetsFetcher(locations=("Argentina",))
+
+        host, port = FLAGS.server.split(':')
+        channel = implementations.insecure_channel(host, int(port))
+        self.stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
     def run_core(self):
         for tweet in self.tweets_stream:
@@ -32,28 +38,16 @@ class ClassifierCore(MagusCore):
         self.out_queue.close()
         return 0
 
+    def make_request(self):
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = 'morgana'
+        request.model_spec.signature_name = 'predict_tweets'
 
-tf.app.flags.DEFINE_string('server', 'localhost:9000',
-                           'PredictionService host:port')
-tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
-FLAGS = tf.app.flags.FLAGS
+        request.inputs['tweet_features'].CopyFrom(
+                tf.contrib.util.make_tensor_proto([[0.1 for _ in range(80)] for _ in range(300)],
+                                                  shape=[1, 80, 300, 1]))
 
-
-def main(_):
-    host, port = FLAGS.server.split(':')
-    channel = implementations.insecure_channel(host, int(port))
-    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-    # Send request
-    # See prediction_service.proto for gRPC request/response details.
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'morgana'
-    request.model_spec.signature_name = 'predict_tweets'
-
-    # x_text, y = AttardiCNNSchema.get_input_data()
-
-    request.inputs['tweet_features'].CopyFrom(
-            tf.contrib.util.make_tensor_proto([[0.1 for _ in range(80)] for _ in range(300)], shape=[1, 80, 300, 1]))
-    result = stub.Predict(request, 10.0)  # 10 secs timeout
-    result = tensor_util.MakeNdarray(result.outputs["scores"])[0]
-    for i in range(len(result)):
-        print(EMOTION_LOOKUP[i], ":", result[i])
+        result = self.stub.Predict(request, 10.0)  # 10 secs timeout
+        result = tensor_util.MakeNdarray(result.outputs["scores"])[0]
+        for i in range(len(result)):
+            print(EMOTION_LOOKUP[i], ":", result[i])
