a164fdf991e26e38d4462ca05b714c1519cf0709
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-20 00:34:52 -0300

    Updated old libs

--- src/libs/neural_classifier.py
+++ src/libs/neural_classifier.py
@@ -1,7 +1,5 @@
 import random
 
-from libs.sentiments_handling import ANGRY, HAPPY, NONE, SAD
-
 INPUT_SIZE = 3
 INPUT_LEN = 3
 OUTPUT_LEN = 9
@@ -13,5 +11,4 @@ class NeuralClassifier():
 
     def classify(self, input_matrix):
         # Baseline
-        # return [random.random() for _ in range(OUTPUT_LEN)]
-        return random.choice([HAPPY, ANGRY, SAD, HAPPY, ANGRY, SAD, HAPPY, ANGRY, SAD, NONE])
+        return [random.random() for _ in range(OUTPUT_LEN)]
