4bc91757db750231b60a26275225ba587663aa0e
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-21 04:22:52 -0300

    Added dump core Updated core world embedding

--- src/m_cores/core_word_embeding.py
+++ src/m_cores/core_word_embeding.py
@@ -5,9 +5,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 from libs.embeddings_handler import EmbeddingHandler
 from libs.tweet_parser import TweetParser
 from m_cores.magus_core import MagusCore
+from m_cores.magus_dump_core import MagusDumpCore
 
 
-class WordEmbeddingCore(MagusCore):
+class WordEmbeddingCore(MagusDumpCore):
     def __init__(self, input_queue, output_queue, tag="Word Embedding", worker_number=0):
         MagusCore.__init__(self, tag, worker_number, input_queue, output_queue)
         self.embeddings = EmbeddingHandler("words", 300, 80)
@@ -25,7 +26,11 @@ class WordEmbeddingCore(MagusCore):
 
             matrix = self.embeddings.get_embedding_matrix(tweet["words"])
 
-            with open("vectors/word_matrix_{}".format(tweet[TweetParser.TWEET_ID])) as matrix_file:
+            matrix_filename = "vectors/word_matrix_{}".format(tweet[TweetParser.TWEET_ID])
+            with open(matrix_filename, 'w') as matrix_file:
                 matrix_file.write(self.serializer.dumps(matrix))
+
+            send_advice = (tweet[TweetParser.TWEET_ID], matrix_filename)
+            self.dump_queue.send_message(send_advice)
 
         self.in_queue.receive_messages(callback)
