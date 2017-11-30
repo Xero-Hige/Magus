7c223b64f35029df63562c9b2d5b7e0e47a7beb7
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-30 16:39:36 -0300

    Added missing file

---
+++ src/lexicons/lexicons.py
@@ -0,0 +1,5 @@
+from libs.lexicon import Lexicon
+
+LEXICONS = [Lexicon("./lexicons/en_lexicon.lx", lang="en"),
+            Lexicon("./lexicons/es_lexicon.lx", lang="es"),
+            Lexicon("./lexicons/pt_lexicon.lx", lang="pt")]
