06565c8f52260f2d5499bbdf6746dabe74decf75
Xero-Hige <gaston.martinez.90@gmail.com>
2018-01-09 15:33:25 -0300

    Updated tokenizers

--- src/tokenizer/base_tokenizer.py
+++ src/tokenizer/base_tokenizer.py
@@ -2,16 +2,24 @@ class Tokenizer():
     """Defines a tokenizer class to parse tweets"""
 
     @staticmethod
-    def preprocess(tweet):
-        """Preprocess the tweet to be in the format to use the tokenizer. It
+    def _text_preprocess(tweet):
+        """Preprocess the tweet text to be in the format to use the tokenizer. It
         only should be used in the trainer."""
 
         raise NotImplementedError
 
     @staticmethod
-    def tokenize(tweet):
+    def tokenize(tweet, preprocessed_text=""):
         """Returns a list of strings, being each string a token of the given
         parsed tweet. Each token appears in the relative order inside the
         tweet given by the tokenize method"""
+
+        raise NotImplementedError
+
+    @staticmethod
+    def tokenize_raw(tweet):
+        """Returns a list of strings, being each string a token of the given
+        parsed tweet. Each token appears in the relative order inside the
+        tweet given by the tokenize method. """
 
         raise NotImplementedError
--- src/tokenizer/char_tokenizer.py
+++ src/tokenizer/char_tokenizer.py
@@ -1,23 +1,31 @@
-from tokenizer.base_tokenizer import Tokenizer
-
 from libs.tweet_anonymize import full_anonymize_tweet
+from tokenizer.base_tokenizer import Tokenizer
 
 WINDOW_SIZE = 3
 
 
 class CharTokenizer(Tokenizer):
     @staticmethod
-    def preprocess(tweet):
-        tweet["tweet_text"] = tweet["tweet_text"].lower()
-        tweet["tweet_text"] = full_anonymize_tweet(tweet["tweet_text"])
+    def _text_preprocess(tweet):
+        preprocessed_text = tweet["tweet_text"].lower()
+        return full_anonymize_tweet(preprocessed_text)
 
     @staticmethod
-    def tokenize(tweet):
+    def tokenize(tweet, preprocessed_text=None):
         result = []
 
-        tweet_text = tweet["tweet_text"]
+        tweet_text = preprocessed_text if preprocessed_text else tweet["tweet_text"]
 
         for i in range(len(tweet_text) - WINDOW_SIZE + 1):
             result.append(tweet_text[i:i + WINDOW_SIZE])
 
         return result
+
+    @staticmethod
+    def tokenize_raw(tweet):
+        """Returns a list of strings, being each string a token of the given
+        parsed tweet. Each token appears in the relative order inside the
+        tweet given by the tokenize method. """
+
+        preprocessed_text = CharTokenizer._text_preprocess(tweet)
+        return CharTokenizer.tokenize(tweet, preprocessed_text)
