d6f975783d43bc6a9da522d84beae685857f461a
Xero-Hige <gaston.martinez.90@gmail.com>
2018-01-10 02:41:48 -0300

    Added embeddings generator for chars and raw chars

--- src/embeddings_generator.py
+++ src/embeddings_generator.py
@@ -1,14 +1,32 @@
 import gensim
 
 from libs.sentence_yield import TrainSentenceGenerator
+from tokenizer.char_tokenizer import CharTokenizer
+from tokenizer.raw_char_tokenizer import RawCharTokenizer
 from tokenizer.word_tokenizer import WordTokenizer
 
 
-def generate_embeddings():
+def generate_word_embeddings():
     sentences = TrainSentenceGenerator(["../tweets", "../bulk"], WordTokenizer)
     model = gensim.models.Word2Vec(sentences, min_count=3, size=300)
     word_vector = model.wv
     word_vector.save('./wordsEmbeddings.mdl')
 
 
-generate_embeddings()
+def generate_raw_chars_embeddings():
+    sentences = TrainSentenceGenerator(["../tweets", "../bulk"], RawCharTokenizer)
+    model = gensim.models.Word2Vec(sentences, min_count=10, size=300)
+    word_vector = model.wv
+    word_vector.save('./rCharsEmbeddings.mdl')
+
+
+def generate_chars_embeddings():
+    sentences = TrainSentenceGenerator(["../tweets", "../bulk"], CharTokenizer)
+    model = gensim.models.Word2Vec(sentences, min_count=10, size=300)
+    word_vector = model.wv
+    word_vector.save('./charsEmbeddings.mdl')
+
+
+generate_word_embeddings()
+generate_raw_chars_embeddings()
+generate_chars_embeddings()
