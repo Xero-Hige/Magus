75b2cce5a707a8592e6aa5d0ac64e3f0e6b409a7
Xero-Hige <gaston.martinez.90@gmail.com>
2018-02-07 17:18:24 -0300

    Still fits (git just broke)

---
+++ src/morgana_cnn_schema.py
@@ -0,0 +1,230 @@
+import numpy as np
+import tensorflow as tf
+
+from cnn_schema import CNNSchema
+from libs.db_tweet import DB_Handler
+from libs.embedding_mapper import EmbeddingMapper
+from libs.sentiments_handling import ANGER, FEAR, JOY, NEUTRAL, SADNESS
+from libs.tweet_parser import TweetParser
+from tokenizer.char_tokenizer import CharTokenizer
+from tokenizer.raw_char_tokenizer import RawCharTokenizer
+from tokenizer.word_tokenizer import WordTokenizer
+
+MAX_WORDS = 120
+MAX_CHARS = 320
+
+EMOTION_LOOKUP = {
+    JOY:     0,
+    # TRUST:        1,
+    FEAR:    1,
+    # SURPRISE:     3,
+    SADNESS: 2,
+    # DISGUST:      5,
+    ANGER:   3,
+    # ANTICIPATION: 7,
+    NEUTRAL: 4
+}
+
+
+class MorganaCNNSchema(CNNSchema):
+    """
+    A CNN for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
+
+    W_MAPPER = EmbeddingMapper("./wordsEmbeddings.mdl", MAX_WORDS, 300)
+    C_MAPPER = EmbeddingMapper("./charsEmbeddings.mdl", MAX_CHARS, 300)
+    R_MAPPER = EmbeddingMapper("./rCharsEmbeddings.mdl", MAX_CHARS, 300)
+
+    def __init__(self, num_classes, vocab_size, embedding_size, filter_sizes, num_filters,
+                 l2_reg_lambda=0.0):
+
+        # Placeholders for input, output and dropout
+        super().__init__(num_classes, vocab_size, embedding_size, filter_sizes, num_filters,
+                         l2_reg_lambda)
+
+        words_input_features, input_labels = self.create_input_layer(num_classes, embedding_size, name="_words")
+        chars_input_features, input_labels = self.create_input_layer(num_classes, embedding_size, name="_chars")
+        r_chars_input_features, input_labels = self.create_input_layer(num_classes, embedding_size, name="_rchars")
+
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+        l2_loss = tf.constant(0.0)
+
+        self.input_x_words = words_input_features
+        self.input_x_chars = chars_input_features
+        self.input_x_rchars = r_chars_input_features
+
+        self.input_y = input_labels
+
+        # TODO: Check
+        output_layers_size = num_classes
+
+        with tf.name_scope("words_stream"):
+            hidden_words, words_dl = self.create_first_combs(self.input_x_words, embedding_size, filter_sizes, l2_loss,
+                                                             num_filters, MAX_WORDS, "words",
+                                                             output_size=output_layers_size,
+                                                             hidden_layer_size=int(output_layers_size * 102.4))
+
+            word_predictions = tf.argmax(words_dl, 1)
+        self.word_loss = self.get_loss(self.input_y, l2_loss, l2_reg_lambda, words_dl)
+        self.word_accuracy = self.get_accuracy(self.input_y, word_predictions)
+
+        with tf.name_scope("chars_stream"):
+            hidden_chars, chars_dl = self.create_first_combs(self.input_x_chars, embedding_size, filter_sizes, l2_loss,
+                                                             num_filters, MAX_CHARS, "chars",
+                                                             output_size=output_layers_size,
+                                                             hidden_layer_size=int(output_layers_size * 102.4))
+
+            char_predictions = tf.argmax(chars_dl, 1)
+        self.char_loss = self.get_loss(self.input_y, l2_loss, l2_reg_lambda, chars_dl)
+        self.char_accuracy = self.get_accuracy(self.input_y, char_predictions)
+
+        with tf.name_scope("rchar_stream"):
+            hidden_rchar, r_chars_dl = self.create_first_combs(self.input_x_rchars, embedding_size, filter_sizes,
+                                                               l2_loss,
+                                                               num_filters, MAX_CHARS, "rchars",
+                                                               output_size=output_layers_size,
+                                                               hidden_layer_size=int(output_layers_size * 102.4))
+
+            rchar_predictions = tf.argmax(r_chars_dl, 1)
+        self.rchar_loss = self.get_loss(self.input_y, l2_loss, l2_reg_lambda, r_chars_dl)
+        self.rchar_accuracy = self.get_accuracy(self.input_y, rchar_predictions)
+
+
+        with tf.name_scope("global"):
+            dense_layers = tf.concat([hidden_words, hidden_chars, hidden_rchar], 1)
+            convolution_layer_output, output_channels = self.create_conv_pool_layer(
+                    tf.reshape(dense_layers, [-1, int(output_layers_size * 102.4) * 3, 1, 1]),
+                    1,
+                    [num_classes * 3],
+                    int(num_classes * 102.4),
+                    num_classes * 3,
+                    activation=tf.nn.tanh)
+
+            dropout = self.create_dropout_layer(convolution_layer_output, dropout_prob=self.dropout_keep_prob)
+
+            scores, predictions = self.create_output_layer(dropout,
+                                                           output_channels,
+                                                           num_classes,
+                                                           l2_loss=l2_loss)
+
+        self.scores = scores
+        self.predictions = predictions
+
+        self.loss = self.get_loss(self.input_y, l2_loss, l2_reg_lambda, scores)
+        self.accuracy = self.get_accuracy(self.input_y, predictions)
+
+    def create_first_combs(self,
+                           input_layer,
+                           embedding_size,
+                           filter_sizes,
+                           l2_loss,
+                           num_filters,
+                           number_of_features,
+                           name,
+                           output_size,
+                           hidden_layer_size=1024):
+
+        convolution_layer_output, output_channels = self.create_conv_pool_layer(input_layer,
+                                                                                embedding_size,
+                                                                                filter_sizes,
+                                                                                num_filters,
+                                                                                number_of_features,
+                                                                                activation=tf.nn.tanh)
+
+        dense_layer = self.create_dense_layer(convolution_layer_output, output_channels, hidden_layer_size,
+                                              name=name, l2_loss=l2_loss)
+
+        dropout = self.create_dropout_layer(dense_layer, dropout_prob=self.dropout_keep_prob)
+
+        redux_dense_layer = self.create_dense_layer(dropout, hidden_layer_size, output_size,
+                                                    name="redux_{}".format(name), l2_loss=l2_loss)
+        return dense_layer, redux_dense_layer
+
+    @staticmethod
+    def get_input_data(embedding_size=300):
+        data = []
+        with DB_Handler() as handler:
+            tagged_tweets = handler.get_all_tagged()
+
+            for tweet_data in tagged_tweets:
+                # TODO: Remove
+
+                """try:
+                    tweet = TweetParser.parse_from_json_file("../bulk/{}.json".format(tweet_data.id))
+                    with open("../bulk/{}.json".format(tweet_data.id)) as f_in:
+                        t_string = f_in.read()
+                except IOError:
+                    try:
+                        tweet = TweetParser.parse_from_json_file("../tweets/{}.json".format(tweet_data.id))
+                        with open("../tweets/{}.json".format(tweet_data.id)) as f_in:
+                            t_string = f_in.read()
+
+                    except IOError:
+                        print("Missing tweet id: ", tweet_data.id)
+                        continue
+
+                if tweet_data.get_tweet_emotion() == NEUTRAL and tweet["tweet_lang"].lower() == "en":
+                    with open("../neutrals/{}.json".format(tweet_data.id), 'w') as output:
+                        output.write(t_string)
+
+                if "fx" not in tweet_data.id:
+                    continue"""
+
+                try:
+                    tweet = TweetParser.parse_from_json_file("../database/{}.json".format(tweet_data.id))
+                except IOError:
+                    continue
+
+                data.append((tweet, tweet_data.get_tweet_emotion()))
+
+        features = []
+        labels = []
+
+        for tweet, tag in data:
+            if tag not in EMOTION_LOOKUP:
+                tag = NEUTRAL
+
+            features.append(str(tweet[TweetParser.TWEET_ID]))
+
+            #            label = [0] * len(EMOTION_LOOKUP)
+            #            label[EMOTION_LOOKUP[tag]] = 1
+            labels.append(EMOTION_LOOKUP[tag])
+
+        features = np.asarray(features, dtype=object)
+        labels = np.asarray(labels, dtype=np.float32)
+
+        return features, labels
+
+    @staticmethod
+    def map_batch(tweets):
+
+        maps_words = []
+        maps_chars = []
+        maps_rchar = []
+
+        for t_id in tweets:
+            """try:
+                tweet = TweetParser.parse_from_json_file("../bulk/{}.json".format(t_id))
+            except IOError:
+                try:
+                    tweet = TweetParser.parse_from_json_file("../tweets/{}.json".format(t_id))
+                except IOError:
+                    print("Missing tweet id: ", t_id)
+                    continue"""
+            try:
+                tweet = TweetParser.parse_from_json_file("../database/{}.json".format(t_id))
+            except IOError:
+                print("ERROR!!!!!!!!!!!!!!!!!!", t_id)
+                continue
+
+            tokens = WordTokenizer.tokenize_raw(tweet)
+            maps_words.append(MorganaCNNSchema.W_MAPPER.map_features(tokens, t_id))
+
+            tokens = CharTokenizer.tokenize_raw(tweet)
+            maps_chars.append(MorganaCNNSchema.C_MAPPER.map_features(tokens, t_id))
+
+            tokens = RawCharTokenizer.tokenize_raw(tweet)
+            maps_rchar.append(MorganaCNNSchema.R_MAPPER.map_features(tokens, t_id))
+
+        return np.asarray(maps_words), np.asarray(maps_chars), np.asarray(maps_rchar)
---
+++ src/morgana_trainer.py
@@ -0,0 +1,340 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Train and export a simple Softmax Regression TensorFlow model.
+The model is from the TensorFlow "MNIST For ML Beginner" tutorial. This program
+simply follows all its training instructions, and uses TensorFlow SavedModel to
+export the trained model with proper signatures that can be loaded by standard
+tensorflow_model_server.
+Usage: mnist_export.py [--training_iteration=x] [--model_version=y] export_dir
+"""
+import datetime
+import os
+import pickle
+import random
+import sys
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.saved_model import builder as saved_model_builder, signature_constants, signature_def_utils, \
+    tag_constants, utils
+from tensorflow.python.util import compat
+
+# training flags
+from morgana_cnn_schema import EMOTION_LOOKUP, MAX_WORDS, MorganaCNNSchema
+
+VOCAB_SIZE = MAX_WORDS
+EMBEDDINGS_LENGTH = 300
+NUM_CLASSES = len(EMOTION_LOOKUP)
+
+tf.app.flags.DEFINE_integer('training_iteration', 1000, 'number of training iterations.')
+tf.app.flags.DEFINE_integer('model_version', 1, 'version number of the model.')
+tf.app.flags.DEFINE_string('work_dir', '/tmp', 'Working directory.')
+
+tf.flags.DEFINE_integer("embedding_dim", 300, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_string("filter_sizes", "7,7,7", "Comma-separated filter sizes (default: '3,4,5')")
+tf.flags.DEFINE_integer("num_filters", 200, "Number of filters per filter size (default: 128)")
+tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
+tf.flags.DEFINE_float("l2_reg_lambda", 0, "L2 regularization lambda (default: 0.0)")
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def batch_iter(x, y, batch_size, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+
+    data_size = len(x)
+    # data = np.array(list(zip(x, y)))
+    data = list(zip(x, y))
+
+    num_batches_per_epoch = int((len(x) - 1) / batch_size) + 1
+
+    # Shuffle the data at each epoch
+    if shuffle:
+        # shuffle_indices = np.random.permutation(np.arange(data_size))
+        # shuffled_data = data[shuffle_indices]
+        random.shuffle(data)
+        shuffled_data = data
+    else:
+        shuffled_data = data
+
+    for batch_num in range(num_batches_per_epoch):
+
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+
+        x_list = []
+        y_list = []
+        for i in range(start_index, end_index):
+            x_list.append(shuffled_data[i][0])
+            y_list.append(shuffled_data[i][1])
+
+        x_list = MorganaCNNSchema.map_batch(x_list)
+
+        yield x_list, y_list
+
+
+def main(_):
+    iterations = int(sys.argv[1])
+    filters = sys.argv[2]
+    n_filters = int(sys.argv[3])
+    output_folder = sys.argv[4]
+    model_version = int(sys.argv[5])
+    start_it = int(sys.argv[6])
+
+    # Train model
+    print('Training model...')
+
+    # Read the data and format it
+
+    sess = tf.InteractiveSession()
+
+    # Build model
+    # x_train, y_train = get_train_data()
+
+    cnn = MorganaCNNSchema(
+            num_classes=NUM_CLASSES,
+            vocab_size=VOCAB_SIZE,
+            embedding_size=FLAGS.embedding_dim,
+            filter_sizes=list(map(int, filters.split(","))),
+            num_filters=n_filters,
+            l2_reg_lambda=FLAGS.l2_reg_lambda)
+
+    train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "global")
+    global_trainer = tf.train.AdamOptimizer(1e-3).minimize(cnn.loss, var_list=train_vars)
+
+    train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "words_stream")
+    word_trainer = tf.train.AdamOptimizer(1e-3).minimize(cnn.word_loss, var_list=train_vars)
+
+    train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "chars_stream")
+    char_trainer = tf.train.AdamOptimizer(1e-3).minimize(cnn.char_loss, var_list=train_vars)
+
+    train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "rchar_stream")
+    rchar_trainer = tf.train.AdamOptimizer(1e-3).minimize(cnn.rchar_loss, var_list=train_vars)
+
+    tf.initialize_all_variables().run()
+
+    # train the model
+    saver = tf.train.Saver()
+
+    if start_it != 0:
+        saver.restore(sess, "tmp/" + output_folder + "_{}.ckpt".format(start_it))
+
+    test_batches = ["/media/hige/320/train_data/train_batch_{}.dmp".format(1)]
+    batches = ["/media/hige/320/train_data/train_batch_{}.dmp".format(2)]
+
+    start_it -= 1 if start_it != 0 else 0
+
+    acc_word = 0
+    acc_char = 0
+    acc_rchar = 0
+    for it in range(start_it + 1, iterations):
+        acc_word, loss_word = do_train_step(batches, cnn, it, output_folder, saver, sess,
+                                            word_trainer, cnn.word_loss, cnn.word_accuracy, name="Words",
+                                            prev_acc=acc_word)
+        acc_char, loss_char = do_train_step(batches, cnn, it, output_folder, saver, sess,
+                                            char_trainer, cnn.char_loss, cnn.char_accuracy, name="Chars",
+                                            prev_acc=acc_char)
+        acc_rchar, loss_rchar = do_train_step(batches, cnn, it, output_folder, saver, sess,
+                                              rchar_trainer, cnn.rchar_loss, cnn.rchar_accuracy, name="Rchar",
+                                              prev_acc=acc_rchar)
+        # do_train_step(batches, cnn, it, output_folder, saver, sess,
+        #              global_trainer, cnn.loss, cnn.accuracy, "Global")
+
+        # if (it % 3) == 0:
+        #    do_test_step(test_batches, cnn, sess)
+
+    do_test_step(test_batches, cnn, sess)
+    print('Done training!')
+
+    # Save the model
+
+    # where to save to?
+    export_path_base = output_folder
+    export_path = os.path.join(
+            compat.as_bytes(export_path_base),
+            compat.as_bytes(str(model_version)))
+
+    print('Exporting trained model to', export_path)
+
+    # This creates a SERVABLE from our model
+    # saves a "snapshot" of the trained model to reliable storage
+    # so that it can be loaded later for inference.
+    # can save as many version as necessary
+
+    # the tensoroflow serving main file tensorflow_model_server
+    # will create a SOURCE out of it, the source
+    # can house state that is shared across multiple servables
+    # or versions
+
+    # we can later create a LOADER from it using tf.saved_model.loader.load
+
+    # then the MANAGER decides how to handle its lifecycle
+
+    builder = saved_model_builder.SavedModelBuilder(export_path)
+
+    # Build the signature_def_map.
+    # Signature specifies what type of model is being exported,
+    # and the input/output tensors to bind to when running inference.
+    # think of them as annotiations on the graph for serving
+    # we can use them a number of ways
+    # grabbing whatever inputs/outputs/models we want either on server
+    # or via client
+    classification_inputs_words = utils.build_tensor_info(cnn.input_x_words)
+    classification_inputs_chars = utils.build_tensor_info(cnn.input_x_chars)
+    classification_inputs_rchar = utils.build_tensor_info(cnn.input_x_rchars)
+    classification_outputs_classes = utils.build_tensor_info(cnn.input_y)
+    classification_outputs_scores = utils.build_tensor_info(cnn.scores)
+
+    classification_signature = signature_def_utils.build_signature_def(
+            inputs={"words": classification_inputs_words,
+                    "chars": classification_inputs_chars,
+                    "rchar": classification_inputs_rchar},
+            outputs={
+                signature_constants.CLASSIFY_OUTPUT_CLASSES:
+                    classification_outputs_classes,
+                signature_constants.CLASSIFY_OUTPUT_SCORES:
+                    classification_outputs_scores
+            },
+            method_name=signature_constants.CLASSIFY_METHOD_NAME)
+
+    tensor_info_scores = utils.build_tensor_info(cnn.scores)
+    tensor_info_predictions = utils.build_tensor_info(cnn.predictions)
+
+    prediction_signature = signature_def_utils.build_signature_def(
+            inputs={"words": classification_inputs_words,
+                    "chars": classification_inputs_chars,
+                    "rchar": classification_inputs_rchar},
+            outputs={'scores': tensor_info_scores, 'predictions': tensor_info_predictions},
+            method_name=signature_constants.PREDICT_METHOD_NAME)
+
+    legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+
+    # add the sigs to the servable
+    builder.add_meta_graph_and_variables(
+            sess, [tag_constants.SERVING],
+            signature_def_map={
+                'predict_tweets':
+                    prediction_signature,
+                signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+                    classification_signature,
+            },
+            legacy_init_op=legacy_init_op)
+
+    # save it!
+    builder.save()
+
+    print('Done exporting!')
+
+
+def do_train_step(batches, cnn, it, output_folder, saver, sess, trainer, trainig_loss, training_accuracy, name="",
+                  prev_acc=1):
+    print("Iteration <{}>".format(name), it)
+    random.shuffle(batches)
+    total_loss = 0
+    total_accuracy = 0
+    total_batches = 0
+    for batch_number in range(len(batches)):
+        with open(batches[batch_number], 'rb') as pickled:
+            batch_data = pickle.load(pickled)
+
+        print("[{}] Iteration{}: {} - Batch: {}/{}".format(datetime.datetime.now(), name, it, batch_number + 1,
+                                                           len(batches)))
+        drop = .75 if prev_acc < .54 else .5
+
+        _, loss, accuracy = sess.run([trainer, trainig_loss, training_accuracy], feed_dict={
+            cnn.input_x_words: batch_data [0],
+            cnn.input_x_chars: batch_data [1],
+            cnn.input_x_rchars: batch_data[2],
+            cnn.input_y: batch_data       [3],
+            cnn.dropout_keep_prob:        drop
+        })
+
+        total_batches += 1
+        total_accuracy += float(accuracy)
+        total_loss += float(loss)
+
+        # if not name:
+        # print('\n####b--{}--b#####\nLoss: {}\nAccuracy: {}\n#####--{}--#####'.format(batch_number,
+        #                                                                              loss,
+        #                                                                              accuracy,
+        #                                                                              batch_number)
+        #       )
+    print('\n##{}##--{}--##{}##\nLoss: {}\nAccuracy: {}\n#####--{}--#####'.format(name, it, name,
+                                                                                  total_loss / total_batches,
+                                                                                  total_accuracy / total_batches,
+                                                                                  it)
+          )
+    save_path = saver.save(sess, "tmp/" + output_folder + "_{}.ckpt".format(it))
+    print("Iteration training end: {}".format(save_path))
+
+    return total_accuracy / total_batches, total_loss / total_batches
+
+
+def do_test_step(batches, cnn, sess):
+    total_loss = 0
+    total_accuracy = 0
+    total_batches = 0
+    random.shuffle(batches)
+    for batch_number in range(len(batches)):
+        with open(batches[batch_number], 'rb') as pickled:
+            batch_data = pickle.load(pickled)
+
+        print("Test batch: {}/{}".format(batch_number, len(batches)))
+        feed_dict = {
+            cnn.input_x_words: batch_data [0],
+            cnn.input_x_chars: batch_data [1],
+            cnn.input_x_rchars: batch_data[2],
+            cnn.input_y: batch_data       [3],
+            cnn.dropout_keep_prob:        1
+        }
+
+        loss, accuracy = sess.run([cnn.loss, cnn.accuracy], feed_dict)
+        total_batches += 1
+        total_accuracy += float(accuracy)
+        total_loss += float(loss)
+    print('\n#####--{}--#####\nLoss: {}\nAccuracy: {}\n#####--{}--#####'.format("X",
+                                                                                total_loss / total_batches,
+                                                                                total_accuracy / total_batches,
+                                                                                "X")
+          )
+
+    return total_accuracy / total_batches, total_loss / total_batches
+
+
+def get_train_data():
+    print("Loading data...")
+    x_text, y = MorganaCNNSchema.get_input_data()
+
+    x = x_text
+
+    np.random.seed(10)
+    shuffle_indices = np.random.permutation(np.arange(len(y)))
+    x_shuffled = x[shuffle_indices]
+    y_shuffled = y[shuffle_indices]
+
+    # dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
+    x_train, x_dev = x_shuffled, x_shuffled  # x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
+    y_train, y_dev = y_shuffled, y_shuffled  # y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+
+    print("Vocabulary Size: {:d}".format(70))
+    print("Train/Dev split: {:d}/{:d}".format(len(x_train), len(x_dev)))
+    return x_train, y_train
+
+
+if __name__ == '__main__':
+    tf.app.run()
