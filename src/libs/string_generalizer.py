b6769760bb84030fafaa966b706a0216ac2ea4c6
Xero-Hige <gaston.martinez.90@gmail.com>
2018-02-21 16:34:00 -0300

    Re enabled ñ parsing at strip accents

--- src/libs/string_generalizer.py
+++ src/libs/string_generalizer.py
@@ -1,12 +1,18 @@
 # Based on: https://gist.github.com/topicus/4611549
 # Updated on 13/11/2017 with ñ skip
 
+import sys
 import unicodedata
 
 
 def strip_accents(s):
     stripped = [c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn']
-    # for i in range(len(stripped)):  TODO: CHECK
-    #    if stripped[i] == 'Ñ' or stripped[i] == 'ñ':
-    #        stripped[i] = s[i]
+    try:
+        for i in range(len(stripped)):
+            if s[i] == 'Ñ' or s[i] == 'ñ':
+                stripped[i] = s[i]
+
+    except IndexError as e:
+        print("Wrong strip: {} caused by {}".format(e, s), file=sys.stderr)
+
     return "".join(stripped)
