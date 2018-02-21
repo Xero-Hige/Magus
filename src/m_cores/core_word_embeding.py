a92bc37106030dccb2a52026d1561d295e3f331f
Xero-Hige <gaston.martinez.90@gmail.com>
2018-02-20 23:11:29 -0300

    Fixed cores errors during dumping and rebuilding vectors

--- src/m_cores/core_word_embeding.py
+++ src/m_cores/core_word_embeding.py
@@ -12,7 +12,7 @@ from m_cores.magus_dump_core import MagusDumpCore
 class WordEmbeddingCore(MagusDumpCore):
     def __init__(self, input_queue, output_queue, tag="Word Embedding", worker_number=0):
         MagusDumpCore.__init__(self, input_queue, output_queue, tag, worker_number)
-        self.embeddings = EmbeddingHandler("words", 300, 120)
+        self.embeddings = EmbeddingHandler("wordsEmbeddings", 300, 120)
 
     def run_core(self):
         def callback(tweet_string):
@@ -28,7 +28,7 @@ class WordEmbeddingCore(MagusDumpCore):
 
             matrix_filename = "vectors/word_matrix_{}.npy".format(tweet[TweetParser.TWEET_ID])
             with open(matrix_filename, 'wb') as matrix_file:
-                numpy.save(matrix_file, matrix, allow_pickle=False, fix_imports=True)
+                numpy.save(matrix_file, matrix, allow_pickle=True, fix_imports=True)
 
             send_advice = (tweet[TweetParser.TWEET_ID], matrix_filename)
             self.dump_queue.send_message(self.serializer.dumps(send_advice))
