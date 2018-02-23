09f6e45dcfe907d6e6cbc654d9d5843fe938fdb6
Xero-Hige <gaston.martinez.90@gmail.com>
2018-02-23 02:25:12 -0300

    Updated embeddings generator to use configs

--- src/embeddings_generator.py
+++ src/embeddings_generator.py
@@ -1,30 +1,31 @@
 import gensim
 
 from libs.sentence_yield import TrainSentenceGenerator
+from morgana_config_handler import EMBEDDINGS_FILES, EMBEDDING_SIZES, TWEETS_DIRS
 from tokenizer.char_tokenizer import CharTokenizer
 from tokenizer.raw_char_tokenizer import RawCharTokenizer
 from tokenizer.word_tokenizer import WordTokenizer
 
 
 def generate_word_embeddings():
-    sentences = TrainSentenceGenerator(["../database"], WordTokenizer)
-    model = gensim.models.Word2Vec(sentences, min_count=3, size=300)
+    sentences = TrainSentenceGenerator(["../{}".format(x) for x in TWEETS_DIRS], WordTokenizer)
+    model = gensim.models.Word2Vec(sentences, min_count=3, size=EMBEDDING_SIZES["words"])
     word_vector = model.wv
-    word_vector.save('./wordsEmbeddings.mdl')
+    word_vector.save('./{}.mdl'.format(EMBEDDINGS_FILES["words"]))
 
 
 def generate_raw_chars_embeddings():
-    sentences = TrainSentenceGenerator(["../database"], RawCharTokenizer)
-    model = gensim.models.Word2Vec(sentences, min_count=5, size=150)
+    sentences = TrainSentenceGenerator(["../{}".format(x) for x in TWEETS_DIRS], RawCharTokenizer)
+    model = gensim.models.Word2Vec(sentences, min_count=5, size=EMBEDDING_SIZES["raw_chars"])
     word_vector = model.wv
-    word_vector.save('./rCharsEmbeddings.mdl')
+    word_vector.save('./{}.mdl'.format(EMBEDDINGS_FILES["raw_chars"]))
 
 
 def generate_chars_embeddings():
-    sentences = TrainSentenceGenerator(["../database"], CharTokenizer)
-    model = gensim.models.Word2Vec(sentences, min_count=5, size=150)
+    sentences = TrainSentenceGenerator(["../{}".format(x) for x in TWEETS_DIRS], CharTokenizer)
+    model = gensim.models.Word2Vec(sentences, min_count=5, size=EMBEDDING_SIZES["chars"])
     word_vector = model.wv
-    word_vector.save('./charsEmbeddings.mdl')
+    word_vector.save('./{}.mdl'.format(EMBEDDINGS_FILES["chars"]))
 
 
 generate_word_embeddings()
