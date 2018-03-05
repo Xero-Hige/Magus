5001424377c508c536c1368a1e83e6731c15770b
Xero-Hige <gaston.martinez.90@gmail.com>
2018-03-04 21:41:47 -0300

    Added missing excluded folders

--- ccontainers.sh
+++ ccontainers.sh
@@ -2,7 +2,7 @@
 
 # remove exited containers:
 docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
-    
+
 # remove unused images:
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
 
