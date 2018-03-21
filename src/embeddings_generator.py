22fc34086ba72aed0746a0cf102a440e248cae98
Xero-Hige <gaston.martinez.90@gmail.com>
2018-03-20 22:18:34 -0300

    Added info to embeddings generator

--- src/embeddings_generator.py
+++ src/embeddings_generator.py
@@ -8,6 +8,7 @@ from tokenizer.word_tokenizer import WordTokenizer
 
 
 def generate_word_embeddings():
+    print("Generating embeddings for words")
     sentences = TrainSentenceGenerator(["../{}".format(x) for x in TWEETS_DIRS], WordTokenizer)
     model = gensim.models.Word2Vec(sentences, min_count=3, size=EMBEDDING_SIZES["words"])
     word_vector = model.wv
@@ -15,6 +16,7 @@ def generate_word_embeddings():
 
 
 def generate_raw_chars_embeddings():
+    print("Generating embeddings for raw_chars")
     sentences = TrainSentenceGenerator(["../{}".format(x) for x in TWEETS_DIRS], RawCharTokenizer)
     model = gensim.models.Word2Vec(sentences, min_count=5, size=EMBEDDING_SIZES["raw_chars"])
     word_vector = model.wv
@@ -22,12 +24,20 @@ def generate_raw_chars_embeddings():
 
 
 def generate_chars_embeddings():
+    print("Generating embeddings for chars")
     sentences = TrainSentenceGenerator(["../{}".format(x) for x in TWEETS_DIRS], CharTokenizer)
     model = gensim.models.Word2Vec(sentences, min_count=5, size=EMBEDDING_SIZES["chars"])
     word_vector = model.wv
     word_vector.save('./{}.mdl'.format(EMBEDDINGS_FILES["chars"]))
 
 
-generate_word_embeddings()
-generate_raw_chars_embeddings()
-generate_chars_embeddings()
+def main():
+    print("Generating embeddings files")
+    generate_word_embeddings()
+    generate_raw_chars_embeddings()
+    generate_chars_embeddings()
+    print("Generating embeddings for words")
+
+
+if __name__ == '__main__':
+    main()
