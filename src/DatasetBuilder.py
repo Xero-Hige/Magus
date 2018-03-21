a8a35185fc92fe740eabec1ced0240c9397dba02
Xero-Hige <gaston.martinez.90@gmail.com>
2018-03-20 21:32:57 -0300

    Added train data folder to config

--- src/DatasetBuilder.py
+++ src/DatasetBuilder.py
@@ -3,7 +3,9 @@ import pickle
 import numpy as np
 
 from morgana_cnn_schema import MorganaCNNSchema
+from morgana_config_handler import TRAIN_DATA_FOLDER
 
+SEED = 97
 SLICE = 50
 
 
@@ -13,7 +15,7 @@ def get_train_data():
 
     x = x_text
 
-    np.random.seed(97)
+    np.random.seed(SEED)
     shuffle_indices = np.random.permutation(np.arange(len(y)))
     x_shuffled = x[shuffle_indices]
     y_shuffled = y[shuffle_indices]
@@ -26,31 +28,35 @@ def get_train_data():
     return x_train, y_train, x_dev, y_dev
 
 
-x_data, y_data, dev_x, dev_y = get_train_data()
+def main():
+    x_data, y_data, dev_x, dev_y = get_train_data()
+
+    i = 0
+    while i * SLICE < len(x_data):
+        print("SLICE {}".format(i))
+        batch = x_data[(i * SLICE):((i + 1) * SLICE)]
+        a, b, c = MorganaCNNSchema.map_batch(batch)
+
+        dump = a, b, c, y_data[(i * SLICE):((i + 1) * SLICE)]
+
+        with open("{}/train_batch_{}.dmp".format(TRAIN_DATA_FOLDER, i), 'wb') as output:
+            pickle.dump(dump, output)
+
+        i += 1
+
+    i = 0
+    while i * SLICE < len(dev_x):
+        print("SLICE {}".format(i))
+        batch = dev_x[(i * SLICE):((i + 1) * SLICE)]
+        a, b, c = MorganaCNNSchema.map_batch(batch)
+
+        dump = a, b, c, dev_y[(i * SLICE):((i + 1) * SLICE)]
+
+        with open("{}/test_batch_{}.dmp".format(TRAIN_DATA_FOLDER, i), 'wb') as output:
+            pickle.dump(dump, output)
+
+        i += 1
 
 
-i = 0
-while i * SLICE < len(x_data):
-    print("SLICE {}".format(i))
-    batch = x_data[(i * SLICE):((i + 1) * SLICE)]
-    a, b, c = MorganaCNNSchema.map_batch(batch)
-
-    dump = a, b, c, y_data[(i * SLICE):((i + 1) * SLICE)]
-
-    with open("/media/hige/320/train_data/train_batch_{}.dmp".format(i), 'wb') as output:
-        pickle.dump(dump, output)
-
-    i += 1
-
-i = 0
-while i * SLICE < len(dev_x):
-    print("SLICE {}".format(i))
-    batch = dev_x[(i * SLICE):((i + 1) * SLICE)]
-    a, b, c = MorganaCNNSchema.map_batch(batch)
-
-    dump = a, b, c, dev_y[(i * SLICE):((i + 1) * SLICE)]
-
-    with open("/media/hige/320/train_data/test_batch_{}.dmp".format(i), 'wb') as output:
-        pickle.dump(dump, output)
-
-    i += 1
+if __name__ == '__main__':
+    main()
