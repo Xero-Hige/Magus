ddf92f430e9331373499b04bb4b025b8c7a32934
Xero-Hige <gaston.martinez.90@gmail.com>
2018-05-03 15:50:00 -0300

    Update missing title param

--- src/the_world.py
+++ src/the_world.py
@@ -9,6 +9,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/', methods=["GET"])
 def map_show():
     return render_template("map.html",
+                           Title="The World",
                            subscribe_key=os.environ["PUBNUB_SUB_KEY"],
                            publish_key=os.environ["PUBNUB_PUB_KEY"],
                            map_access_token=os.environ["MAPBOX_KEY"],
